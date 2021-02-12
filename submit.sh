2b86386a47e7e28b563a92cd5e1dd2a1b082aa9e
RDoerfel <doerfelruben@aol.com>
2021-02-12 16:42:36 +0100

    Maint:test submit.sh

--- submit.sh
+++ submit.sh
@@ -3,19 +3,19 @@
 ### –- specify queue: gpuv100, gputitanxpascal, gpuk40, gpum2050 --
 #BSUB -q gpuv100
 ### -- set the job Name --
-#BSUB -J job_name
+#BSUB -J pbci_eval
 ### -- ask for number of cores (default: 1) --
 #BSUB -n 1
 ### -- Select the resources: 1 gpu in exclusive process mode --
 #BSUB -gpu "num=1:mode=exclusive_process"
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
-#BSUB -W 01:00
+#BSUB -W 20:00
 ### -- request 5GB of memory
 #BSUB -R "rusage[mem=5GB]"
 ### -- set the email address --
 # please uncomment the following line and put in your e-mail address,
 # if you want to receive e-mail notifications on a non-default address
-##BSUB -u your_email_address
+#BSUB -u s202286@student.dtu.dk
 ### -- send notification at start --
 #BSUB -B
 ### -- send notification at completion--
@@ -35,8 +35,7 @@ module load python/3.7.3
 # Load virtual Python environment
 source venv/bin/activate
 
-
 ##################################################################
 # Execute your own code by replacing the sanity check code below #
 ##################################################################
-python -c "import mne; mne.sys_info()"
+python3 hw.py
