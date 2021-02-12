0b3cbe0275d699bc775b506615c4db151c730e0d
RDoerfel <doerfelruben@aol.com>
2021-02-12 16:24:39 +0100

    maint:add further modules for installation

--- setup.sh
+++ setup.sh
@@ -10,4 +10,11 @@ python3 -m venv pbci
 source pbci/bin/activate
 
 # install mne
-pip install --user mne
+pip install -U pip
+pip install -U setuptools
+pip install sklearn
+pip install seaborn
+pip install pandas
+pip install matplotlib
+pip install numpy
+pip install mne
